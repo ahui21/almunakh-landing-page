08fabf7dfba85a9b8b7239e74c32c96fd2ea7fcf
Alwin Hui <alwin.iwillwin@gmail.com>
2024-12-14 17:18:20 -0800

    configure for deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
-    domains: ['i.pravatar.cc'],
+    unoptimized: true,
   },
+  basePath: '',
+  assetPrefix: '',
 }
 
 module.exports = nextConfig 
